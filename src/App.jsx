7614f3f1c282d9bc123690e0d0cc9ebf3d63cefa
Harsh Prajapati <harshprajapati07ce@gmail.com>
2025-09-14 11:36:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Radhe 💛 Krishna</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is my {count} ${import.meta.env.VITE_ENV}
+          count is my {count} {import.meta.env.VITE_ENV}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
