f585849852d85c2c193bc5dd46b0a9af6ba3de88
Harsh Prajapati <harshprajapati07ce@gmail.com>
2025-09-14 11:45:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  console.log(${import.meta.env.VITE_ENV})
 
   return (
     <>
@@ -16,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Radhe 💛 Krishna</h1>
+      <h1>Radhe 💛 Krishna  {import.meta.env.VITE_ENV}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is my {count} {import.meta.env.VITE_ENV}
